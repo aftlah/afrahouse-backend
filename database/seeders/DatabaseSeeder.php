f041ba90e9e08c18cbc2e3ad9f5346d4b402317e
aftlah <altaf.fattah05@gmail.com>
2024-10-20 11:25:13 +0700

    add: Transaction

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Listing;
+use App\Models\Transaction;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,14 +16,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Admin Afrehouse',
             'email' => 'altaf@gmail.com',
-            'role'=>'admin',
+            'role' => 'admin',
         ]);
 
+
+        $user = User::factory(10)->create();
         $listing = Listing::factory()->count(10)->create();
+
+        Transaction::factory(10)
+            ->state(
+                new Sequence(
+                    fn(Sequence $sequence) => [
+                        'user_id' => $user->random(),
+                        'listing_id' => $listing->random(),
+                    ]
+                )
+            )->create();
     }
 }
